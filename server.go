6381eca1de88f9de4d7b38239c28812f8c901926
lukechampine <luke.champine@gmail.com>
2019-06-02 19:19:12 -0400

    DRYer server code

---
+++ server.go
@@ -0,0 +1,352 @@
+package walrus
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"strconv"
+	"unsafe"
+
+	"github.com/julienschmidt/httprouter"
+	"gitlab.com/NebulousLabs/Sia/crypto"
+	"gitlab.com/NebulousLabs/Sia/encoding"
+	"gitlab.com/NebulousLabs/Sia/modules"
+	"gitlab.com/NebulousLabs/Sia/types"
+	"lukechampine.com/us/wallet"
+)
+
+func writeJSON(w io.Writer, v interface{}) {
+	// encode nil slices as [] instead of null
+	if val := reflect.ValueOf(v); val.Kind() == reflect.Slice && val.Len() == 0 {
+		w.Write([]byte("[]\n"))
+		return
+	}
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "\t")
+	enc.Encode(v)
+}
+
+type genericWallet interface {
+	Addresses() []types.UnlockHash
+	AddressInfo(addr types.UnlockHash) (wallet.SeedAddressInfo, bool)
+	Balance() types.Currency
+	ChainHeight() types.BlockHeight
+	ConsensusChangeID() modules.ConsensusChangeID
+	LimboOutputs() []wallet.LimboOutput
+	MarkSpent(id types.SiacoinOutputID, spent bool)
+	Memo(txid types.TransactionID) []byte
+	OwnsAddress(addr types.UnlockHash) bool
+	SetMemo(txid types.TransactionID, memo []byte)
+	Transaction(id types.TransactionID) (types.Transaction, bool)
+	Transactions(n int) []types.TransactionID
+	TransactionsByAddress(addr types.UnlockHash, n int) []types.TransactionID
+	UnspentOutputs() []wallet.UnspentOutput
+}
+
+type genericServer struct {
+	w  genericWallet
+	tp wallet.TransactionPool
+}
+
+func (s *genericServer) addressesHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	writeJSON(w, s.w.Addresses())
+}
+
+func (s *genericServer) addressesaddrHandlerGET(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	var addr types.UnlockHash
+	if err := addr.LoadString(ps.ByName("addr")); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	info, ok := s.w.AddressInfo(addr)
+	if !ok {
+		http.Error(w, "No such entry", http.StatusNotFound)
+		return
+	}
+	writeJSON(w, responseAddressesAddr(info))
+}
+
+func (s *genericServer) balanceHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	writeJSON(w, s.w.Balance())
+}
+
+func (s *genericServer) broadcastHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	var txnSet []types.Transaction
+	if err := json.NewDecoder(req.Body).Decode(&txnSet); err != nil {
+		http.Error(w, "Could not parse transaction: "+err.Error(), http.StatusBadRequest)
+		return
+	} else if len(txnSet) == 0 {
+		http.Error(w, "Transaction set is empty", http.StatusBadRequest)
+		return
+	}
+
+	// add any unconfirmed parents of the first transaction in the set
+	parents := wallet.UnconfirmedParents(txnSet[0], s.tp)
+
+	// submit the transaction set (ignoring duplicate error -- if the set is
+	// already in the tpool, great)
+	err := s.tp.AcceptTransactionSet(append(parents, txnSet...))
+	if err != nil && err != modules.ErrDuplicateTransactionSet {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// mark all wallet-owned inputs as spent
+	// TODO: wouldn't need this if wallet was subscribed to tpool
+	for _, txn := range txnSet {
+		for _, sci := range txn.SiacoinInputs {
+			if s.w.OwnsAddress(sci.UnlockConditions.UnlockHash()) {
+				s.w.MarkSpent(sci.ParentID, true)
+			}
+		}
+	}
+}
+
+func (s *genericServer) consensusHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	writeJSON(w, ResponseConsensus{
+		Height: s.w.ChainHeight(),
+		CCID:   crypto.Hash(s.w.ConsensusChangeID()),
+	})
+}
+
+func (s *genericServer) feeHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	median, _ := s.tp.FeeEstimation()
+	writeJSON(w, median)
+}
+
+func (s *genericServer) limboHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	writeJSON(w, responseLimboUTXOs(s.w.LimboOutputs()))
+}
+
+func (s *genericServer) limboHandlerPUT(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	var id crypto.Hash
+	if err := id.LoadString(ps.ByName("id")); err != nil {
+		http.Error(w, "Invalid ID: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	s.w.MarkSpent(types.SiacoinOutputID(id), true)
+}
+
+func (s *genericServer) limboHandlerDELETE(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	var id crypto.Hash
+	if err := id.LoadString(ps.ByName("id")); err != nil {
+		http.Error(w, "Invalid ID: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	s.w.MarkSpent(types.SiacoinOutputID(id), false)
+}
+
+func (s *genericServer) memosHandlerPUT(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	var txid crypto.Hash
+	if err := txid.LoadString(ps.ByName("txid")); err != nil {
+		http.Error(w, "Invalid transaction ID: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, "Couldn't read memo: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	s.w.SetMemo(types.TransactionID(txid), body)
+}
+
+func (s *genericServer) memosHandlerGET(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	var txid crypto.Hash
+	if err := txid.LoadString(ps.ByName("txid")); err != nil {
+		http.Error(w, "Invalid transaction ID: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Write(s.w.Memo(types.TransactionID(txid)))
+}
+
+func (s *genericServer) transactionsHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	max := -1 // all txns
+	if req.FormValue("max") != "" {
+		var err error
+		max, err = strconv.Atoi(req.FormValue("max"))
+		if err != nil {
+			http.Error(w, "Could not decode request body: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+
+	var resp []types.TransactionID
+	if req.FormValue("addr") != "" {
+		var addr types.UnlockHash
+		if err := addr.LoadString(req.FormValue("addr")); err != nil {
+			http.Error(w, "Invalid address: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		resp = s.w.TransactionsByAddress(addr, max)
+	} else {
+		resp = s.w.Transactions(max)
+	}
+	writeJSON(w, resp)
+}
+
+func (s *genericServer) transactionsidHandler(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	var txid crypto.Hash
+	if err := txid.LoadString(ps.ByName("txid")); err != nil {
+		http.Error(w, "Invalid transaction ID: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	txn, ok := s.w.Transaction(types.TransactionID(txid))
+	if !ok {
+		http.Error(w, "Transaction not found", http.StatusNotFound)
+		return
+	}
+	// calculate inflow/outflow/fee
+	var inflow, outflow, fee types.Currency
+	for _, sco := range txn.SiacoinOutputs {
+		if s.w.OwnsAddress(sco.UnlockHash) {
+			inflow = inflow.Add(sco.Value)
+		} else {
+			outflow = outflow.Add(sco.Value)
+		}
+	}
+	for _, c := range txn.MinerFees {
+		fee = fee.Add(c)
+	}
+	outflow = outflow.Add(fee)
+	writeJSON(w, ResponseTransactionsID{
+		Transaction: txn,
+		Inflow:      inflow,
+		Outflow:     outflow,
+		FeePerByte:  fee.Div64(uint64(txn.MarshalSiaSize())),
+	})
+}
+
+func (s *genericServer) utxosHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	outputs := s.w.UnspentOutputs()
+	utxos := make([]UTXO, len(outputs))
+	for i, o := range outputs {
+		info, ok := s.w.AddressInfo(o.UnlockHash)
+		if !ok {
+			panic("missing info for " + o.UnlockHash.String())
+		}
+		utxos[i] = UTXO{
+			ID:               o.ID,
+			Value:            o.Value,
+			UnlockConditions: info.UnlockConditions,
+			UnlockHash:       o.UnlockHash,
+			KeyIndex:         info.KeyIndex,
+		}
+	}
+	writeJSON(w, utxos)
+}
+
+// Generic API
+
+func newGenericServer(w genericWallet, tp wallet.TransactionPool) *httprouter.Router {
+	s := genericServer{
+		w:  w,
+		tp: tp,
+	}
+	mux := httprouter.New()
+	mux.GET("/addresses", s.addressesHandler)
+	mux.GET("/addresses/:addr", s.addressesaddrHandlerGET)
+	mux.GET("/balance", s.balanceHandler)
+	mux.POST("/broadcast", s.broadcastHandler)
+	mux.GET("/consensus", s.consensusHandler)
+	mux.GET("/fee", s.feeHandler)
+	mux.PUT("/limbo/:id", s.limboHandlerPUT)
+	mux.GET("/limbo", s.limboHandler)
+	mux.DELETE("/limbo/:id", s.limboHandlerDELETE)
+	mux.PUT("/memos/:txid", s.memosHandlerPUT)
+	mux.GET("/memos/:txid", s.memosHandlerGET)
+	mux.GET("/transactions", s.transactionsHandler)
+	mux.GET("/transactions/:txid", s.transactionsidHandler)
+	mux.GET("/utxos", s.utxosHandler)
+	return mux
+}
+
+// Hot Wallet API
+
+type seedServer struct {
+	w *wallet.SeedWallet
+}
+
+func (s *seedServer) nextaddressHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	writeJSON(w, s.w.NextAddress())
+}
+
+func (s *seedServer) seedindexHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	writeJSON(w, s.w.SeedIndex())
+}
+
+func (s *seedServer) signHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	var rs RequestSign
+	if err := json.NewDecoder(req.Body).Decode(&rs); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	} else if err := s.w.SignTransaction(&rs.Transaction, rs.ToSign); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	writeJSON(w, (*encodedTransaction)(unsafe.Pointer(&rs.Transaction)))
+}
+
+// NewSeedServer returns an HTTP handler that serves the seed wallet API.
+func NewSeedServer(w *wallet.SeedWallet, tp wallet.TransactionPool) http.Handler {
+	s := &seedServer{w}
+	mux := newGenericServer(w, tp)
+	mux.POST("/nextaddress", s.nextaddressHandler)
+	mux.GET("/seedindex", s.seedindexHandler)
+	mux.POST("/sign", s.signHandler)
+	return mux
+}
+
+// Watch-Only Wallet API
+
+// need to override (WatchOnlyWallet).AddressInfo to satisfy genericWallet
+type watchSeedInfoWallet struct {
+	*wallet.WatchOnlyWallet
+}
+
+func (w watchSeedInfoWallet) AddressInfo(addr types.UnlockHash) (wallet.SeedAddressInfo, bool) {
+	info := w.WatchOnlyWallet.AddressInfo(addr)
+	if info == nil {
+		return wallet.SeedAddressInfo{}, false
+	}
+	var entry wallet.SeedAddressInfo
+	if err := encoding.Unmarshal(info, &entry); err != nil {
+		panic(err)
+	}
+	return entry, true
+}
+
+type watchSeedServer struct {
+	w *wallet.WatchOnlyWallet
+}
+
+func (s *watchSeedServer) addressesHandlerPOST(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	var info wallet.SeedAddressInfo
+	if err := json.NewDecoder(req.Body).Decode(&info); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	addr := info.UnlockConditions.UnlockHash()
+	s.w.AddAddress(addr, encoding.Marshal(info))
+	writeJSON(w, addr)
+}
+
+func (s *watchSeedServer) addressesaddrHandlerDELETE(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	var addr types.UnlockHash
+	if err := addr.LoadString(ps.ByName("addr")); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	s.w.RemoveAddress(addr)
+}
+
+// NewWatchSeedServer returns an HTTP handler that serves the watch-only
+// seed-based wallet API.
+func NewWatchSeedServer(w *wallet.WatchOnlyWallet, tp wallet.TransactionPool) http.Handler {
+	s := &watchSeedServer{w}
+	mux := newGenericServer(watchSeedInfoWallet{w}, tp)
+	mux.POST("/addresses", s.addressesHandlerPOST)
+	mux.DELETE("/addresses/:addr", s.addressesaddrHandlerDELETE)
+	return mux
+}
